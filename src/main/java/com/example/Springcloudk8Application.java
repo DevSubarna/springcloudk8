30a8782813a1a6b446dba8fd307b8d7b65274c4b
DevSubarna <59051752+DevSubarna@users.noreply.github.com>
2022-06-04 11:09:54 -0500

    intial commit services

---
+++ src/main/java/com/example/Springcloudk8Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloudk8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springcloudk8Application.class, args);
+	}
+
+}
