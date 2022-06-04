13b032514eff66cd960f78d7a7f1cc02e534e81a
DevSubarna <barnacse03@gmail.com>
2022-06-04 17:44:59 -0500

    commit service

--- account/src/main/java/com/example/account/AccountApplication.java
+++ account/src/main/java/com/example/account/AccountApplication.java
@@ -2,10 +2,19 @@ package com.example.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AccountApplication {
+    @GetMapping("/message")
+    public String displayMessage(){
+        return "Congratulation you successfully deployed your application to kubernetes !!";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class, args);
     }
+
 }
